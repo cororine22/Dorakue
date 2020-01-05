9704961fbd4c0e1855125729746c661ca3709c9c
cororine22 <cororine22.prb2@gmail.com>
2020-01-05 16:41:28 +0900

    random initialize param

--- main.rb
+++ main.rb
@@ -1,27 +1,52 @@
 class Brave
-  # nameのセッター
-  def name=(name)
+
+  # new演算子から渡された引数を受け取る
+  def initialize(name,hp,offense,defense)
     @name = name
-  end
-
-  # hpのセッター
-  def hp=(hp)
     @hp = hp
-  end
-
-  # offenseのセッター
-  def offense=(offense)
     @offense = offense
-  end
-
-  # defenseのセッター
-  def defense=(defense)
     @defense = defense
   end
+
+  def name
+    @name
+  end
+
+  def hp
+    @hp
+  end
+
+  def offense
+    @offense
+  end
+
+  def defense
+    @defense
+  end
+
+  # # nameのセッター
+  # def name=(name)
+  #   @name = name
+  # end
+
+  # # hpのセッター
+  # def hp=(hp)
+  #   @hp = hp
+  # end
+
+  # # offenseのセッター
+  # def offense=(offense)
+  #   @offense = offense
+  # end
+
+  # # defenseのセッター
+  # def defense=(defense)
+  #   @defense = defense
+  # end
 end
 
 # 勇者クラスをインスタンス化
-brave = Brave.new
+brave = Brave.new("テリー",500,150,100)
 
 # パラメータに値を設定 ヒアドキュメント
 puts <<~TEXT

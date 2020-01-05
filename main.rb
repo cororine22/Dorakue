6001e56d577b9a3d2f1ddb0d611a9d429124822e
cororine22 <cororine22.prb2@gmail.com>
2020-01-05 18:23:43 +0900

    fix getter

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 class Brave
+  # attr_readerの記述でゲッターを省略することができる
+  attr_reader :name, :hp, :offense, :defense
 
   # new演算子から渡された引数を受け取る
   def initialize(**params)
@@ -8,42 +10,6 @@ class Brave
     @offense = params[:offense]
     @defense = params[:defense]
   end
-
-  def name
-    @name
-  end
-
-  def hp
-    @hp
-  end
-
-  def offense
-    @offense
-  end
-
-  def defense
-    @defense
-  end
-
-  # # nameのセッター
-  # def name=(name)
-  #   @name = name
-  # end
-
-  # # hpのセッター
-  # def hp=(hp)
-  #   @hp = hp
-  # end
-
-  # # offenseのセッター
-  # def offense=(offense)
-  #   @offense = offense
-  # end
-
-  # # defenseのセッター
-  # def defense=(defense)
-  #   @defense = defense
-  # end
 end
 
 # 勇者クラスをインスタンス化

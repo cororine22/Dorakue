253e79927cc2a57ddd0854e256766a830001215b
cororine22 <cororine22.prb2@gmail.com>
2020-01-14 12:13:56 +0900

    fix games_controller.rb(set Instance variables)

--- games_controller.rb
+++ games_controller.rb
@@ -6,42 +6,51 @@ class GamesController
 
     # バトルの処理
     def battle(**params)
-        brave = params[:brave]
-        monster = params[:monster]
+        # build_charactersを呼び出し
+        build_characters(params)
 
         loop do
-        brave.attack(monster)
-        break if buttle_end?(monster)
-        monster.attack(brave)
-        break if buttle_end?(brave)
+        @brave.attack(@monster)
+        break if buttle_end?
+        @monster.attack(@brave)
+        break if buttle_end?
         end
         # 勇者の勝敗によってメッセージを変える
-        if battle_result(brave)
-            result = calculate_of_exp_and_gold(monster)
-            puts "#{brave.name}は戦いに勝った"
+        if battle_result
+            result = calculate_of_exp_and_gold
+            puts "#{@brave.name}は戦いに勝った"
             puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
         else
-            puts "#{brave.name}は戦いに負けた"
+            puts "#{@brave.name}は戦いに負けた"
             puts "目の前が真っ暗になった"
         end
     end
 
     # 以下のメソッドはクラス外から呼び出す必要がないのでprivate以下に記述する
     private
+
+        # キャラクターのインスタンスをインスタンス変数に格納
+        def build_characters(**params)
+            @brave = params[:brave]
+            @monster = params[:monster]
+
+        end
+
         # バトル終了の判定
-        def buttle_end?(character)
-            character.hp <= 0
+        def buttle_end?
+            # 勇者かモンスター、どちらかのHPが0になったらバトルが終了する
+            @brave.hp <= 0 || @monster.hp <= 0
         end
 
         # 勇者の勝利判定
-        def battle_result(brave)
-            brave.hp > 0
+        def battle_result
+            @brave.hp > 0
         end
 
         # 経験値とゴールドの計算
-        def calculate_of_exp_and_gold(monster)
-            exp = (monster.offense + monster.defense) * EXP_CONSTANT
-            gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+        def calculate_of_exp_and_gold
+            exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+            gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
             result = {exp: exp, gold: gold}
 
             result
